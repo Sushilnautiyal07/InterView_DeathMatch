04d54ce2e2366d52c89a6c79afa327cb2d70e2ae
Sushil Nautiyal <nautiyals927@gmail.com>
2026-02-27 21:17:44 +0530

    java class auto fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
-import {serve} from 'inngest/express';
-import {clerkMiddleware} from "@clerk/express";
+import { serve } from 'inngest/express';
+import { clerkMiddleware } from "@clerk/express";
 import chatRoutes from './routes/chatRoutes.js';
 
-import {ENV} from './lib/env.js';
-import {connectDB} from './lib/db.js';
-import {inngest ,functions} from './lib/inngest.js';
+import { ENV } from './lib/env.js';
+import { connectDB } from './lib/db.js';
+import { inngest, functions } from './lib/inngest.js';
 
 
 const app = express();
@@ -21,14 +21,19 @@ app.use(cors({
 }));
 app.use(clerkMiddleware());
 
-app.use("/api/inngest", serve({client: inngest, functions}));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
 
 // CODE RUN ROUTE (piston proxy)
 app.post("/api/run", async (req, res) => {
   try {
     const { language, files } = req.body;
-    const code = files?.[0]?.content;
+    let code = files?.[0]?.content;
+
+    // 🔥 Java class name auto-fix
+    if (language === "java") {
+      code = code.replace(/public class\s+\w+/, "public class Main");
+    }
 
     const languageMap = {
       javascript: 63,
@@ -84,7 +89,7 @@ app.get('/health', (req, res) => {
   res.status(200).json({ msg: 'api is running' });
 });
 
-if(ENV.NODE_ENV === 'production') {
+if (ENV.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
   app.get('/{*any}', (req, res) => {
     res.redirect(ENV.CLIENT_URL);
@@ -95,17 +100,17 @@ if(ENV.NODE_ENV === 'production') {
 
 
 const startServer = async () => {
-  
-try {
-  await  connectDB();
-  app.listen(ENV.PORT, () => {
-  console.log(`Server is running on port ${ENV.PORT}`);
- 
-});
-}catch (error) {
-  console.error(' ❌Failed to start server', error);
-  process.exit(1);
-}
+
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log(`Server is running on port ${ENV.PORT}`);
+
+    });
+  } catch (error) {
+    console.error(' ❌Failed to start server', error);
+    process.exit(1);
+  }
 };
 
 startServer();
